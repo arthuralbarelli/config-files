822bc3cad32e3a01b2ba853e21ea18507ea09818
Arthur Albarelli <arthuralbarelli@gmail.com>
2023-09-20 16:33:49 -0300

    feat(nvim): Add completion setting

---
+++ .config/nvim/lua/user/cmp.lua
@@ -0,0 +1,40 @@
+local cmp_status_ok, cmp = pcall(require, 'cmp')
+if not cmp_status_ok then
+	return
+end
+
+local snip_status_ok, lua_snip = pcall(require, 'luasnip')
+if not snip_status_ok then
+	return
+end
+
+cmp.setup({
+    snippet = {
+      expand = function(args)
+        luasnip.lsp_expand(args.body) -- For `luasnip` users.
+      end,
+    },
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-k>'] = cmp.mapping.select_prev_item(),
+			['<C-j>'] = cmp.mapping.select_next_item(),
+			['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      -- { name = 'vsnip' }, -- For vsnip users.
+      { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
