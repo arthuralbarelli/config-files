0ab5fafbb6f9cf61014f2e8de7edb742725dd28c
Arthur Albarelli <arthuralbarelli@gmail.com>
2023-09-24 20:34:51 -0300

    feat(nvim): Add autopairs plugin

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -10,7 +10,6 @@ local ensure_packer = function()
 end
 
 local packer_bootstrap = ensure_packer()
-
 vim.cmd([[
   augroup packer_user_config
     autocmd!
@@ -20,35 +19,46 @@ vim.cmd([[
 
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-	return
+  return
 end
 
 return require('packer').startup(function(use)
   use "wbthomason/packer.nvim"
-	use "nvim-lua/plenary.nvim"
-	use "nvim-lua/popup.nvim"
-	use "folke/tokyonight.nvim"
+  use "nvim-lua/plenary.nvim"
+  use "nvim-lua/popup.nvim"
+  use "folke/tokyonight.nvim"
 
 
-	-- cmp plugins
-	use 'hrsh7th/cmp-buffer'
-	use 'hrsh7th/cmp-path'
-	use 'hrsh7th/cmp-cmdline'
-	use 'hrsh7th/nvim-cmp'
-	use 'saadparwaiz1/cmp_luasnip'
+  -- cmp plugins
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+  use 'saadparwaiz1/cmp_luasnip'
 
-	-- snippets plugins
-	use	"L3MON4D3/LuaSnip"
-	use "rafamadriz/friendly-snippets"
+  -- snippets plugins
+  use({
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    tag = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!:).
+    run = "make install_jsregexp"
+  })
+  use "rafamadriz/friendly-snippets"
 
-	-- treesitter plugin
-	    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = function()
-            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-            ts_update()
-        end,
-    }
+  -- treesitter plugin
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
+  }
+
+  use {
+    "windwp/nvim-autopairs",
+    config = function() require("nvim-autopairs").setup {} end
+  }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
