2c463f18c2ad27f79352bce0c300955ad149a5a9
Arthur Albarelli <arthuralbarelli@gmail.com>
2023-10-13 22:39:23 -0300

    feat(nvim): Add lsp-config plugin

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -35,6 +35,7 @@ return require('packer').startup(function(use)
   use 'hrsh7th/cmp-cmdline'
   use 'hrsh7th/nvim-cmp'
   use 'saadparwaiz1/cmp_luasnip'
+  use 'neovim/nvim-lspconfig'
 
   -- snippets plugins
   use({
