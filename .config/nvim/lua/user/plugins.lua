740cd0a4d71385167fd1b2a6f0819d09987fd696
Arthur Albarelli <arthuralbarelli@gmail.com>
2023-10-15 23:19:45 -0300

    feat(nvim): Add prettier plugin

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -68,6 +68,7 @@ return require('packer').startup(function(use)
   }
 
   use 'windwp/nvim-ts-autotag'
+  use 'MunifTanjim/prettier.nvim'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
